eac192037f530ec09752c79a4b59f5a6901f8083
Dima <zmitrook@yandex.ru>
2020-12-17 14:33:28 +0100

    first commit1

---
+++ index/parll.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("mousemove", parallax);
+function parallax(e) {
+    document.querySelectorAll(".nav").forEach(function (move) {
+        var moving_value = move.getAttribute("data-value");
+        var x = (e.clientX * moving_value) / 150
+        var y = (e.clientY * moving_value) / 150
+
+
+        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+
+    });
+}
+
+
